d7bf53c6c339e9516a35184d9a04127b5d5fffc7
Johan Larsson <johan_gavle1@hotmail.com>
2014-11-03 22:27:15 +0100

    Squash -> home

---
+++ Gu.Wpf.Localization/DesigntimeAsserts.cs
@@ -0,0 +1,17 @@
+﻿namespace Gu.Wpf.Localization
+{
+    using System;
+    using System.Linq;
+
+    public static class DesigntimeAsserts
+    {
+        private static readonly ResxTranslationProvider Provider = new ResxTranslationProvider(AppDomain.CurrentDomain.GetAssemblies().Where(x => !x.IsDynamic));
+        public static void AssertTranslation(string key)
+        {
+            //if (!Provider.HasKey(key, null))
+            //{
+            //    throw new ArgumentException(string.Format("No translation for {0}", key));
+            //}
+        }
+    }
+}
