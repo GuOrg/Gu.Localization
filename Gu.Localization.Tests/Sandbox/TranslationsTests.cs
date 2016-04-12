2526f2be4ec3f4c76b1cfb7843fab916e7728f85
Johan Larsson <johan_gavle1@hotmail.com>
2016-04-12 23:35:50 +0200

    Sandbox get cultures by file and folder.

--- Gu.Localization.Tests/Sandbox/TranslationsTests.cs
+++ Gu.Localization.Tests/Sandbox/TranslationsTests.cs
@@ -1,4 +1,6 @@
-﻿namespace Gu.Localization.Tests.Sandbox
+﻿using System.IO;
+
+namespace Gu.Localization.Tests.Sandbox
 {
     using System;
     using System.Diagnostics;
@@ -16,6 +18,34 @@
             Console.WriteLine($"Getting cultures took {sw.Elapsed.TotalMilliseconds.ToString("F2")} ms");
             Assert.AreEqual(Properties.Resources.ResourceManager.BaseName, translations.BaseName);
             CollectionAssert.AreEqual(new[] { "de", "en", "sv" }, translations.Cultures.Select(x => x.TwoLetterISOLanguageName));
+        }
+
+        [Test]
+        public void FindCultureFiles()
+        {
+            var sw = Stopwatch.StartNew();
+            var assembly = this.GetType().Assembly;
+            var uri = new Uri(assembly.CodeBase, UriKind.Absolute);
+            var resourceName = $"{assembly.GetName().Name}.resources.dll";
+            var files = Directory.EnumerateFiles(System.IO.Path.GetDirectoryName(uri.LocalPath), resourceName, SearchOption.AllDirectories).ToArray();
+            Console.WriteLine($"Getting {files.Length} cultures took {sw.Elapsed.TotalMilliseconds.ToString("F2")} ms");
+            //CollectionAssert.AreEqual(new[] { "de", "en", "sv" }, translations.Cultures.Select(x => x.TwoLetterISOLanguageName));
+        }
+
+        [Test]
+        public void FindCultureFolders()
+        {
+            var sw = Stopwatch.StartNew();
+            var assembly = this.GetType().Assembly;
+            var uri = new Uri(assembly.CodeBase, UriKind.Absolute);
+            var resourceName = $"{assembly.GetName().Name}.resources.dll";
+            var directories = Directory.EnumerateDirectories(System.IO.Path.GetDirectoryName(uri.LocalPath)).ToArray();
+            foreach (var directory in directories)
+            {
+                Directory.EnumerateFiles(directory, resourceName, SearchOption.TopDirectoryOnly).Any();
+            }
+            Console.WriteLine($"Getting {directories.Length} cultures took {sw.Elapsed.TotalMilliseconds.ToString("F2")} ms");
+            //CollectionAssert.AreEqual(new[] { "de", "en", "sv" }, translations.Cultures.Select(x => x.TwoLetterISOLanguageName));
         }
     }
 }
