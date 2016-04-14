bd0d129bfd575c3a179478e823da9b798e45cbdd
Johan Larsson <johan_gavle1@hotmail.com>
2016-04-14 15:27:43 +0200

    Fix build warnings for StaticExtension.
    - Remove StaticExtensionConverter, no idea if it was ever used for anything.

--- Gu.Wpf.Localization/StaticExtension.cs
+++ Gu.Wpf.Localization/StaticExtension.cs
@@ -1,7 +1,6 @@
 ﻿namespace Gu.Wpf.Localization
 {
     using System;
-    using System.ComponentModel;
     using System.Resources;
     using System.Windows.Data;
     using System.Windows.Markup;
@@ -15,63 +14,39 @@
     /// l:Static p:Resources.YourKey
     /// </summary>
     [MarkupExtensionReturnType(typeof(BindingExpression))]
-    [ContentProperty("Member")]
-    [DefaultProperty("Member")]
-    [TypeConverter(typeof(StaticExtensionConverter))]
     public class StaticExtension : System.Windows.Markup.StaticExtension
     {
+        /// <summary> Initializes a new instance of the <see cref="StaticExtension"/> class.</summary>
+        public StaticExtension()
+        {
+        }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="StaticExtension"/> class using the provided <paramref name="member"/> string.
         /// </summary>
         /// <param name="member">
-        /// A string that identifies the member to make a reference to. This string uses the format prefix:typeName.fieldOrPropertyName. prefix is the mapping prefix for a XAML namespace, and is only required to reference static values that are not mapped to the default XAML namespace.
+        /// A string that identifies the member to make a reference to. This string uses the format prefix:typeName.fieldOrPropertyName.
+        /// prefix is the mapping prefix for a XAML namespace, and is only required to reference static values that are not mapped to the default XAML namespace.
         /// </param>
         /// <exception cref="T:System.ArgumentNullException">
         /// <paramref name="member"/> is null.
         /// </exception>
         public StaticExtension(string member)
+            : base(member)
         {
-            this.Member = member;
         }
 
-        public StaticExtension(string member, ResourceManager resourceManager)
-        {
-            this.Member = member;
-            this.ResourceManager = resourceManager;
-        }
-
-        [ConstructorArgument("member")]
-        public string Member { get; set; }
-
-        public ResourceManager ResourceManager { get; set; }
-
-        /// <summary>
-        /// Returns an object value to set on the property where you apply this extension. For <see cref="T:System.Windows.Markup.StaticExtension"/>, the return value is the static value that is evaluated for the requested static member.
-        /// </summary>
-        /// <returns>
-        /// The static value to set on the property where the extension is applied.
-        /// </returns>
-        /// <param name="serviceProvider">
-        /// An object that can provide services for the markup extension. The service provider is expected to provide a service that implements a type resolver (<see cref="T:System.Windows.Markup.IXamlTypeResolver"/>).
-        /// </param>
-        /// <exception cref="T:System.InvalidOperationException">
-        /// The <see cref="Member"/> value for the extension is null at the time of evaluation.
-        /// </exception>
-        /// <exception cref="T:System.ArgumentException">
-        /// Some part of the <see cref="Member"/> string did not parse properly-or-<paramref name="serviceProvider"/> did not provide a service for <see cref="T:System.Windows.Markup.IXamlTypeResolver"/>-or-<see cref="Member"/> value did not resolve to a static member.
-        /// </exception>
-        /// <exception cref="T:System.ArgumentNullException">
-        /// <paramref name="serviceProvider"/> is null.
-        /// </exception>
+        /// <inheritdoc />
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            if (this.ResourceManager != null)
-            {
-                return CreateBindingExpression(this.ResourceManager, this.Member, serviceProvider);
-            }
-
             try
             {
+                if (this.MemberType != null)
+                {
+                    var resourceManager = ResourceManagers.ForType(this.MemberType);
+                    return CreateBindingExpression(resourceManager, this.Member, serviceProvider);
+                }
+
                 if (string.IsNullOrEmpty(this.Member))
                 {
                     throw new InvalidOperationException("MarkupExtensionStaticMember");
@@ -89,9 +64,9 @@
                     return string.Format(Resources.MissingResourcesFormat, this.Member);
                 }
 
-                this.ResourceManager = ResourceManagers.ForType(type);
+                var manager = ResourceManagers.ForType(type);
                 this.Member = qnk.Key;
-                return CreateBindingExpression(this.ResourceManager, this.Member, serviceProvider);
+                return CreateBindingExpression(manager, this.Member, serviceProvider);
             }
             catch (Exception)
             {
