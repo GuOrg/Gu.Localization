71f7209fc8712944cac13caef79fa172b3fc3c17
Johan Larsson <johan_gavle1@hotmail.com>
2016-04-14 13:42:37 +0200

    More docs.
    - Language
    - LanguageSelector

--- Gu.Wpf.Localization/Language.cs
+++ Gu.Wpf.Localization/Language.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.ComponentModel;
+    using System.Diagnostics;
     using System.Globalization;
     using System.Linq;
     using System.Runtime.CompilerServices;
@@ -10,16 +11,21 @@
 
     using JetBrains.Annotations;
 
+    /// <summary>Class exposing a couple of binding friendly properties for a <see cref="CultureInfo"/></summary>
+    [DebuggerDisplay("Language: {Culture.DisplayName}")]
+    //// ReSharper disable once ClassWithVirtualMembersNeverInherited.Global
     public class Language : INotifyPropertyChanged
     {
         private CultureInfo culture;
-
         private Uri flagSource;
 
+        /// <summary> Initializes a new instance of the <see cref="Language"/> class.</summary>
         public Language()
         {
         }
 
+        /// <summary>Initializes a new instance of the <see cref="Language"/> class.</summary>
+        /// <param name="culture">The culture</param>
         public Language(CultureInfo culture)
         {
             this.culture = culture;
@@ -28,6 +34,7 @@
         /// <inheritdoc />
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>Gets or sets the <see cref="CultureInfo"/></summary>
         public CultureInfo Culture
         {
             get
@@ -43,19 +50,22 @@
                 }
 
                 this.culture = value;
-                this.OnPropertyChanged("");
+                this.OnPropertyChanged(string.Empty);
             }
         }
 
+        /// <summary>Gets or sets a value indicating whether gets or sets if the <see cref="Culture"/> is the same as <see cref="Translator.CurrentCulture"/></summary>
         public bool IsSelected
         {
-            get { return CultureInfoComparer.Default.Equals(Translator.CurrentCulture, this.Culture); }
+            get
+            {
+                return CultureInfoComparer.Default.Equals(Translator.CurrentCulture, this.Culture);
+            }
 
             set
             {
                 if (value == this.IsSelected)
                 {
-
                     this.OnPropertyChanged();
                     return;
                 }
@@ -70,6 +80,7 @@
             }
         }
 
+        /// <summary>Gets a value indicating whether gets a value indicating wheter the <see cref="Culture"/> can be used as <see cref="Translator.CurrentCulture"/></summary>
         public bool CanSelect
         {
             get
@@ -83,6 +94,9 @@
             }
         }
 
+        /// <summary>
+        /// Gets or sets the <see cref="Uri"/> to the flag for the <see cref="Culture"/>
+        /// </summary>
         public Uri FlagSource
         {
             get
@@ -102,15 +116,12 @@
             }
         }
 
-        public string Name => this.culture?.Name;
-
-        public string EnglishName => this.culture?.EnglishName;
-
+        /// <summary>Gets <see cref="Culture"/> NativeName Titlecased and trimmed to text only</summary>
         public string LanguageName
         {
             get
             {
-                var indexOf = this.NativeName.IndexOf(" (");
+                var indexOf = this.NativeName.IndexOf(" (", StringComparison.Ordinal);
                 if (indexOf > 0)
                 {
                     return this.NativeName.Substring(0, indexOf);
@@ -120,34 +131,30 @@
             }
         }
 
-        public string NativeName
-        {
-            get
-            {
-                if (this.culture == null)
-                {
-                    return string.Empty;
-                }
+        /// <summary>Gets <see cref="Culture"/> NativeName Titlecased</summary>
+        public string NativeName => ToFirstCharUpper(this.culture?.NativeName);
 
-                var nativeName = this.culture.NativeName;
-                if (char.IsUpper(nativeName[0]))
-                {
-                    return nativeName;
-                }
-
-                return nativeName.Substring(0, 1).ToUpper() + nativeName.Substring(1);
-            }
-        }
-
-        public override string ToString()
-        {
-            return this.EnglishName;
-        }
-
+        /// <summary>Raises <see cref="PropertyChanged"/></summary>
+        /// <param name="propertyName">The name of the property</param>
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private static string ToFirstCharUpper(string text)
+        {
+            if (string.IsNullOrEmpty(text) || text.Length == 1)
+            {
+                return text;
+            }
+
+            if (char.IsUpper(text[0]))
+            {
+                return text;
+            }
+
+            return $"{char.ToUpper(text[0])}{text.Substring(1)}";
         }
     }
 }
