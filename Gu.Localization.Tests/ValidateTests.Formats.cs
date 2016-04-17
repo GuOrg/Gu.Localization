38789d821cbf84507993ad0efbac5bc46606c364
Johan Larsson <johan_gavle1@hotmail.com>
2016-04-17 22:43:01 +0200

    wip

---
+++ Gu.Localization.Tests/ValidateTests.Formats.cs
@@ -0,0 +1,20 @@
+﻿namespace Gu.Localization.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    public partial class ValidateTests
+    {
+        public class Formats
+        {
+            [TestCase("Hej")]
+            [TestCase("First: {0}, Second: {0}")]
+            public void ThrowsOneArgument(string format)
+            {
+                Assert.Throws<FormatException>(() => Validate.Format(format, 1));
+                Assert.IsFalse(format, Validate.IsValidFormat(format, 1));
+            }
+        }
+    }
+}
--- Gu.Localization/Validate.Formats.cs
+++ Gu.Localization/Validate.Formats.cs
@@ -4,7 +4,7 @@
 
     public partial class Validate
     {
-        internal static void Format(string format, object arg)
+        public static void Format<T>(string format, T arg)
         {
             int count;
             bool? anyItemHasFormat;
@@ -13,10 +13,26 @@
                 throw new FormatException($"Invalid format string: {format}.");
             }
 
+            throw new NotImplementedException("check if T is IFormattable if(anyItemHasFormat == true) ");
+
             if (count != 1)
             {
                 throw new FormatException($"Invalid format string: {format} for the single argument: {arg}.");
             }
+        }
+
+        public static bool IsValidFormat<T>(string format, T arg)
+        {
+            int count;
+            bool? anyItemHasFormat;
+            if (!FormatString.IsValidFormat(format, out count, out anyItemHasFormat))
+            {
+                return false;
+            }
+
+            throw new NotImplementedException("check if T is IFormattable if(anyItemHasFormat == true) ");
+
+            return count == 1;
         }
     }
 }
