87893c35a92be42929ea6e5355156ee2231988ca
Johan Larsson <johan_gavle1@hotmail.com>
2016-04-21 08:48:44 +0200

    Update AssemblyInfo

--- Gu.Localization/Properties/AssemblyInfo.cs
+++ Gu.Localization/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Gu.Localization")]
-[assembly: AssemblyDescription("For localization of WPF applications.")]
+[assembly: AssemblyDescription("For localization using ResourceManager.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Johan Larsson")]
 [assembly: AssemblyProduct("Gu.Localization")]
