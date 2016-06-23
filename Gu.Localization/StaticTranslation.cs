528b6a4f5c97d01710830aa13cae93f6da90d180
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-24 01:31:37 +0200

    Fix code analysis nag.

--- Gu.Localization/StaticTranslation.cs
+++ Gu.Localization/StaticTranslation.cs
@@ -2,11 +2,16 @@ namespace Gu.Localization
 {
     using System.ComponentModel;
     using System.Globalization;
+    using System.Runtime.CompilerServices;
+    using JetBrains.Annotations;
 
     /// <summary>A fake translation that can be used fo nonlocalized strings.</summary>
     public class StaticTranslation : ITranslation
     {
-        /// <summary>Initializes a new instance of the <see cref="StaticTranslation"/> class.</summary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StaticTranslation"/> class.
+        /// Note that <see cref="Translated"/> does not change when <see cref="Translator.EffectiveCulture"/> changed.
+        /// </summary>
         /// <param name="translated">
         /// The text will be used as <see cref="Translated"/> and returned for every culture.
         /// </param>
@@ -15,7 +20,10 @@ namespace Gu.Localization
         {
         }
 
-        /// <summary>Initializes a new instance of the <see cref="StaticTranslation"/> class.</summary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StaticTranslation"/> class.
+        /// Note that <see cref="Translated"/> does not change when <see cref="Translator.EffectiveCulture"/> changed.
+        /// </summary>
         /// <param name="translated">
         /// The text will be used as <see cref="Translated"/> and returned for every culture.
         /// </param>
@@ -31,7 +39,7 @@ namespace Gu.Localization
         /// <inheritdoc />
         public event PropertyChangedEventHandler PropertyChanged;
 
-        /// <inheritdoc />
+        /// <summary>Gets the ~translated~ value. Note that this value does not change when <see cref="Translator.EffectiveCulture"/> changed.</summary>
         public string Translated { get; }
 
         /// <inheritdoc />
@@ -40,10 +48,21 @@ namespace Gu.Localization
         /// <inheritdoc />
         public ErrorHandling ErrorHandling { get; }
 
-        /// <inheritdoc />
+        /// <summary>Returns <see cref="Translated"/> for all inputs.</summary>
+        /// <param name="culture">The culture is ignored.</param>
+        /// <param name="errorHandlingStrategy">The errorhandling is ignored.</param>
+        /// <returns>Returns <see cref="Translated"/>.</returns>
         public string Translate(CultureInfo culture, ErrorHandling errorHandlingStrategy = ErrorHandling.Inherit)
         {
             return this.Translated;
+        }
+
+        /// <summary>Use this to notify about a change of the value for the property named <paramref name="propertyName"/>.</summary>
+        /// <param name="propertyName">The name of the property.</param>
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
