58319b4661ae38f303b54870af0f382daf548625
Johan Larsson <johan_gavle1@hotmail.com>
2017-01-30 12:38:18 +0100

    Bump version.

--- Gu.Localization/Properties/AssemblyInfo.cs
+++ Gu.Localization/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Gu.Localization")]
 [assembly: AssemblyDescription("For localization using ResourceManager.")]
 [assembly: AssemblyConfiguration("")]
@@ -14,27 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0686a7de-9e74-44f6-bfaa-342c73cea820")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.0.0")]
-[assembly: AssemblyFileVersion("6.1.0.0")]
+[assembly: AssemblyVersion("6.2.0.0")]
+[assembly: AssemblyFileVersion("6.2.0.0")]
 [assembly: InternalsVisibleTo("Gu.Wpf.Localization", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Localization.Tests", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Localization.Benchmarks", AllInternalsVisible = true)]
