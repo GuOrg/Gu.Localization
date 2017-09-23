dfd5090888b53c9b0f3528b6bd93904d179026dd
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-23 17:58:07 +0200

    Sign, fix #25.

--- Gu.Wpf.Localization/Properties/AssemblyInfo.cs
+++ Gu.Wpf.Localization/Properties/AssemblyInfo.cs
@@ -13,19 +13,13 @@ using System.Windows.Markup;
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8b5013e4-ebfd-48b4-b6b4-34d6df4fb982")]
 
 [assembly: AssemblyVersion("6.2.0.0")]
 [assembly: AssemblyFileVersion("6.2.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: InternalsVisibleTo("Gu.Wpf.Localization.Tests", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Gu.Wpf.Localization.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001008F3FADDAC2AF5D3A713943D1661FF19562CB93322F985E0A9EE5EA8FBA5D2DE865E27EF3F90B85AE33EABE89449BA2EF391046E9E55080F4E895AA51C298A810445F8C2E00A3EDE5AD9750801E0120421F2747BEDC112D082001C10F3255E33CC7DC4D0263792B195CFCF935C5D723EB85A4906C4B9C5B9D1FC980DE1594FE98", AllInternalsVisible = true)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("http://gu.se/Localization", "Gu.Wpf.Localization")]
 [assembly: XmlnsPrefix("http://gu.se/Localization", "l")]
