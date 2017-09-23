9fef63d114b8219ebd91c1323db31f70acd7987d
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-23 17:37:41 +0200

    Update dependencies.

--- Gu.Wpf.Localization.UiTests/Helpers/StartInfo.cs
+++ Gu.Wpf.Localization.UiTests/Helpers/StartInfo.cs
@@ -1,37 +1,30 @@
 ﻿namespace Gu.Wpf.Localization.UiTests
 {
-    using System;
     using System.Diagnostics;
     using System.Globalization;
-    using System.IO;
-    using System.Reflection;
+    using Gu.Wpf.UiAutomation;
 
     public static class StartInfo
     {
-        public static ProcessStartInfo DemoProject { get; } = CreateStartUpInfo("Gu.Wpf.Localization.Demo", CultureInfo.GetCultureInfo("en"));
+        public static ProcessStartInfo DemoProject { get; } = CreateStartUpInfo(
+            Application.FindExe("Gu.Wpf.Localization.Demo.exe"),
+            CultureInfo.GetCultureInfo("en"));
 
-        public static ProcessStartInfo WithNeutralLanguageProject { get; } = CreateStartUpInfo("Gu.Wpf.Localization.WithNeutralLanguage", CultureInfo.GetCultureInfo("en"));
+        public static ProcessStartInfo WithNeutralLanguageProject { get; } = CreateStartUpInfo(
+            Application.FindExe("Gu.Wpf.Localization.WithNeutralLanguage.exe"),
+            CultureInfo.GetCultureInfo("en"));
 
-        private static ProcessStartInfo CreateStartUpInfo(string assemblyName, CultureInfo culture)
+        private static ProcessStartInfo CreateStartUpInfo(string exeFileName, CultureInfo culture)
         {
             var processStartInfo = new ProcessStartInfo
             {
                 Arguments = culture.Name,
-                FileName = GetExeFileName(assemblyName),
+                FileName = exeFileName,
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
                 RedirectStandardError = true
             };
             return processStartInfo;
-        }
-
-        private static string GetExeFileName(string assemblyName)
-        {
-            var testAssembnly = Assembly.GetExecutingAssembly();
-            var testDirestory = Path.GetDirectoryName(new Uri(testAssembnly.CodeBase).AbsolutePath);
-            var exeDirectory = testDirestory.Replace(testAssembnly.GetName().Name, assemblyName);
-            var fileName = Path.Combine(exeDirectory, $"{assemblyName}.exe");
-            return fileName;
         }
     }
 }
