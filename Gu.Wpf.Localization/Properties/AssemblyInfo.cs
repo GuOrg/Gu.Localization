70c16e3602b1e7ea0c1a2c9ab154b7f5bec9fa1b
Johan Larsson <johan_gavle1@hotmail.com>
2017-10-04 16:35:02 +0200

    Bump version.

--- Gu.Wpf.Localization/Properties/AssemblyInfo.cs
+++ Gu.Wpf.Localization/Properties/AssemblyInfo.cs
@@ -16,8 +16,8 @@ using System.Windows.Markup;
 [assembly: ComVisible(false)]
 [assembly: Guid("8b5013e4-ebfd-48b4-b6b4-34d6df4fb982")]
 
-[assembly: AssemblyVersion("6.2.1.0")]
-[assembly: AssemblyFileVersion("6.2.1.0")]
+[assembly: AssemblyVersion("6.2.2.0")]
+[assembly: AssemblyFileVersion("6.2.2.0")]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: InternalsVisibleTo("Gu.Wpf.Localization.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001008F3FADDAC2AF5D3A713943D1661FF19562CB93322F985E0A9EE5EA8FBA5D2DE865E27EF3F90B85AE33EABE89449BA2EF391046E9E55080F4E895AA51C298A810445F8C2E00A3EDE5AD9750801E0120421F2747BEDC112D082001C10F3255E33CC7DC4D0263792B195CFCF935C5D723EB85A4906C4B9C5B9D1FC980DE1594FE98", AllInternalsVisible = true)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+#### 6.2.2.0
+* BUGFIX: Exclude SourceLink dependency in packages.
+
 #### 6.2.1.0
 * FEATURE: sign
