ec5d40137f449f21495f4f676ada1e6da17fcf3d
JohanLarsson <johan_gavle1@hotmail.com>
2018-04-07 11:16:02 +0200

    Use paket.

--- Gu.Wpf.Localization.Demo.Fody/App.xaml.cs
+++ Gu.Wpf.Localization.Demo.Fody/App.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
 namespace Gu.Wpf.Localization.Demo.Fody
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+    using System.Windows;
+
     public partial class App : Application
     {
     }
--- Gu.Wpf.Localization.Demo.Fody/MainWindow.xaml.cs
+++ Gu.Wpf.Localization.Demo.Fody/MainWindow.xaml.cs
@@ -1,28 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace Gu.Wpf.Localization.Demo.Fody
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    using System.Windows;
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
