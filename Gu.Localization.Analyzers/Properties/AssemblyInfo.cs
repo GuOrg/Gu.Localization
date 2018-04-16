301dd09d17faefa80aa5903c0356b0b47f188b66
JohanLarsson <johan_gavle1@hotmail.com>
2018-04-16 14:41:26 +0200

    Stub analyzer and test project.

---
+++ Gu.Localization.Analyzers/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gu.Localization.Analyzers")]
+[assembly: AssemblyDescription("Roslyn analyzers for Gu.Localization.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Johan Larsson")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0-dev")]
+[assembly:InternalsVisibleTo("Gu.Localization.Analyzers.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001008F3FADDAC2AF5D3A713943D1661FF19562CB93322F985E0A9EE5EA8FBA5D2DE865E27EF3F90B85AE33EABE89449BA2EF391046E9E55080F4E895AA51C298A810445F8C2E00A3EDE5AD9750801E0120421F2747BEDC112D082001C10F3255E33CC7DC4D0263792B195CFCF935C5D723EB85A4906C4B9C5B9D1FC980DE1594FE98")]
