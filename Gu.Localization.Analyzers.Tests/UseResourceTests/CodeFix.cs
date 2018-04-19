78a69c959620f1c8b57ce6e057f316d6bf2a7c0a
Johan Larsson <johan_gavle1@hotmail.com>
2018-04-19 19:34:01 +0200

    Update dependencies.

--- Gu.Localization.Analyzers.Tests/UseResourceTests/CodeFix.cs
+++ Gu.Localization.Analyzers.Tests/UseResourceTests/CodeFix.cs
@@ -1,9 +1,6 @@
 namespace Gu.Localization.Analyzers.Tests.UseResourceTests
 {
     using System.IO;
-    using System.Linq;
-    using System.Threading;
-    using System.Threading.Tasks;
     using Gu.Roslyn.Asserts;
     using Microsoft.CodeAnalysis.CodeFixes;
     using Microsoft.CodeAnalysis.Diagnostics;
@@ -14,17 +11,29 @@ namespace Gu.Localization.Analyzers.Tests.UseResourceTests
         private static readonly DiagnosticAnalyzer Analyzer = new LiteralAnalyzer();
         private static readonly CodeFixProvider Fix = new UseResourceFix();
         private static readonly ExpectedDiagnostic ExpectedDiagnostic = ExpectedDiagnostic.Create("GULOC03");
+        private FileInfo projectFile;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var original = CodeFactory.FindProjectFile("Gu.Localization.TestStub.csproj");
+            var tempDir = Path.Combine(Path.GetTempPath(), original.Directory.Name);
+            if (Directory.Exists(tempDir))
+            {
+                Directory.Delete(tempDir, recursive: true);
+            }
+
+            Copy(original.Directory, new DirectoryInfo(tempDir));
+            this.projectFile = new FileInfo(Path.Combine(tempDir, original.Name));
+        }
 
         [Explicit]
         [Test]
-        public async Task ForDebug()
+        public void ForDebug()
         {
-            var projectFile = CodeFactory.FindProjectFile("Gu.Localization.TestStub.csproj");
-            Directory.Move();
-            var sln = CodeFactory.CreateSolution(projectFile, MetadataReferences.FromAttributes());
-            sln = sln.WithProjectFilePath(sln.ProjectIds[0], projectFile.FullName);
-            var diagnosticsAsync = await Analyze.GetDiagnosticsAsync(sln, Analyzer).ConfigureAwait(false);
-            await HackFix.ApplyAsync(sln, Fix, diagnosticsAsync.Single().Single(), null, CancellationToken.None).ConfigureAwait(false);
+            var sln = CodeFactory.CreateSolution(this.projectFile, MetadataReferences.FromAttributes());
+            var diagnosticsAsync = Analyze.GetDiagnostics(sln, Analyzer);
+            var fixedSln = Roslyn.Asserts.Fix.Apply(sln, Fix, diagnosticsAsync, fixTitle: "Move to resources.");
         }
 
         private static void Copy(DirectoryInfo source, DirectoryInfo target)
