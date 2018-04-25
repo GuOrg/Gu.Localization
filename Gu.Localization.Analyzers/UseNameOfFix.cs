1bfcd0b45e0e327dd2939bc2416ffb0ac9e44f15
JohanLarsson <johan_gavle1@hotmail.com>
2018-04-25 13:50:23 +0200

    Bump version analyzer.

--- Gu.Localization.Analyzers/UseNameOfFix.cs
+++ Gu.Localization.Analyzers/UseNameOfFix.cs
@@ -3,7 +3,7 @@ namespace Gu.Localization.Analyzers
     using System.Collections.Immutable;
     using System.Composition;
     using System.Threading.Tasks;
-    using Gu.Analyzers;
+    using Gu.Roslyn.CodeFixExtensions;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CodeFixes;
     using Microsoft.CodeAnalysis.CSharp;
