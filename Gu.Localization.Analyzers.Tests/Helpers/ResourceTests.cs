78a508d9b74e2c13892f8495b2fcce93d4f4ede2
JohanLarsson <johan_gavle1@hotmail.com>
2018-05-16 14:42:43 +0200

    Bump version analyzer.

--- Gu.Localization.Analyzers.Tests/Helpers/ResourceTests.cs
+++ Gu.Localization.Analyzers.Tests/Helpers/ResourceTests.cs
@@ -7,6 +7,8 @@ namespace Gu.Localization.Analyzers.Tests.Helpers
         [TestCase("Value", "Value")]
         [TestCase("Long 123456789abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRST123456789abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRST text", "Long_123456789abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRST123456789abcdefghijklmnopqrstuvxyzABCDEFG")]
         [TestCase("Some value", "Some_value")]
+        [TestCase("Some\r\nvalue", "Some_n_value")]
+        [TestCase("Some\nvalue", "Some_n_value")]
         [TestCase("The value is: {0}", "The_value_is____0__")]
         [TestCase("The first value is: {0} and the second value is {1}", "The_first_value_is____0___and_the_second_value_is___1__")]
         public void TryGetKey(string text, string expected)
