c77c0a4750114ca9939e97f3cefd69eb9d944ec9
JohanLarsson <johan_gavle1@hotmail.com>
2018-05-17 08:21:34 +0200

    Use correct tokens.

--- Gu.Localization.Analyzers/Fixes/RenameResourceFix.cs
+++ Gu.Localization.Analyzers/Fixes/RenameResourceFix.cs
@@ -110,9 +110,7 @@ namespace Gu.Localization.Analyzers
                 if (token.Parent is PropertyDeclarationSyntax propertyDeclaration &&
                     propertyDeclaration.Identifier == token)
                 {
-                    return SyntaxFactory.ParseToken(this.newValue)
-                                        .WithLeadingTrivia(token.LeadingTrivia)
-                                        .WithTrailingTrivia(token.TrailingTrivia);
+                    return SyntaxFactory.Identifier(token.LeadingTrivia, this.newValue, token.TrailingTrivia);
                 }
 
                 return base.VisitToken(token);
@@ -127,7 +125,7 @@ namespace Gu.Localization.Analyzers
                     invocation.TryGetMethodName(out var method) &&
                     method == "GetString")
                 {
-                    return node.WithToken(SyntaxFactory.ParseToken($"\"{this.newValue}\""));
+                    return node.WithToken(SyntaxFactory.Literal(this.newValue));
                 }
 
                 return base.VisitLiteralExpression(node);
