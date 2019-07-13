9bac8d321d0e5475cad6eb74d64780a8ee52b0f3
Johan Larsson <johan_gavle1@hotmail.com>
2019-07-13 10:16:09 +0200

    Suppress warning.

--- Gu.Wpf.Localization/MarkupExtensions/CurrentCultureProxy.cs
+++ Gu.Wpf.Localization/MarkupExtensions/CurrentCultureProxy.cs
@@ -15,7 +15,9 @@ namespace Gu.Wpf.Localization
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+#pragma warning disable GU0073 // Member of non-public type should be internal. Must be public for binding to work.
         public CultureInfo Value => Translator.CurrentCulture;
+#pragma warning restore GU0073 // Member of non-public type should be internal.
 
         protected virtual void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
         {
