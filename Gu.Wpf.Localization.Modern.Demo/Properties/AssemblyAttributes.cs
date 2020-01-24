5506fd966fe6d9a28cb185d009eb8f2efe7939ad
JohanLarsson <johan_gavle1@hotmail.com>
2020-01-24 16:22:58 +0100

    Use new csproj.

---
+++ Gu.Wpf.Localization.Modern.Demo/Properties/AssemblyAttributes.cs
@@ -0,0 +1,5 @@
+using System.Windows.Media;
+
+// required to support per-monitor DPI awareness in Windows 8.1+
+// see also https://mui.codeplex.com/wikipage?title=Per-monitor%20DPI%20awareness
+[assembly: DisableDpiAwareness]
