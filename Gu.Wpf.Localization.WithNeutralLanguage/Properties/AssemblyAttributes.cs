68403104769e1805bfd74ca7535920caafb61fb0
JohanLarsson <johan_gavle1@hotmail.com>
2020-01-24 16:31:51 +0100

    Use new csproj.

---
+++ Gu.Wpf.Localization.WithNeutralLanguage/Properties/AssemblyAttributes.cs
@@ -0,0 +1,6 @@
+using System.Resources;
+using System.Windows;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.MainAssembly)]
+
