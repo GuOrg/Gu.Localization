27832d5331823327589fdc7678e4f8ce3313cd6c
JohanLarsson <johan_gavle1@hotmail.com>
2020-01-25 17:12:37 +0100

    Fix warnings.

--- Gu.Localization.Analyzers/Helpers/XamlFile.cs
+++ Gu.Localization.Analyzers/Helpers/XamlFile.cs
@@ -1,5 +1,6 @@
 namespace Gu.Localization.Analyzers
 {
+    using System.Globalization;
     using System.IO;
     using System.Text;
     using System.Text.RegularExpressions;
@@ -19,19 +20,16 @@ namespace Gu.Localization.Analyzers
 
         internal static XamlFile Create(string fileName)
         {
-            using (var reader = new StreamReader(File.OpenRead(fileName), Encoding.UTF8, detectEncodingFromByteOrderMarks: true))
-            {
-                return new XamlFile(reader.ReadToEnd(), reader.CurrentEncoding);
-            }
+            using var reader = new StreamReader(File.OpenRead(fileName), Encoding.UTF8, detectEncodingFromByteOrderMarks: true);
+            return new XamlFile(reader.ReadToEnd(), reader.CurrentEncoding);
         }
 
         internal static bool TryUpdateUsage(string fileName, IPropertySymbol resource, string newName, out XamlFile result)
         {
             var xaml = XamlFile.Create(fileName);
             var pattern = $"xmlns:(?<alias>\\w+)=\"clr-namespace:{resource.ContainingType.ContainingSymbol}(\"|;)";
-            if (Regex.Match(xaml.Text, pattern) is Match match &&
-                match.Success &&
-                match.Groups["alias"].Value is string alias &&
+            if (Regex.Match(xaml.Text, pattern) is { Success: true } match &&
+                match.Groups["alias"].Value is { } alias &&
                 !string.IsNullOrEmpty(alias))
             {
                 var oldProperty = $"{alias}:{resource.ContainingType.Name}.{resource.Name}";
@@ -53,8 +51,8 @@ namespace Gu.Localization.Analyzers
         private static string Replace(string xaml, string format, string oldProperty, string newProperty)
         {
             return xaml.Replace(
-                string.Format(format, oldProperty),
-                string.Format(format, newProperty));
+                string.Format(CultureInfo.InvariantCulture, format, oldProperty),
+                string.Format(CultureInfo.InvariantCulture, format, newProperty));
         }
     }
 }
