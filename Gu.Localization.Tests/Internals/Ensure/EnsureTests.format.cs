1bf64ee515aed1b21674639a434912c7146d91e1
JohanLarsson <johan_gavle1@hotmail.com>
2020-06-24 20:20:38 +0200

    Simplify.

--- Gu.Localization.Tests/Internals/Ensure/EnsureTests.format.cs
+++ Gu.Localization.Tests/Internals/Ensure/EnsureTests.format.cs
@@ -7,82 +7,59 @@ namespace Gu.Localization.Tests.Internals
 
     public class EnsureTests
     {
-        private static readonly FormatData[] Valids = new[]
+        private static readonly TestCaseData[] Valids = new[]
         {
-            new FormatData("No argument", null),
-            new FormatData("No argument", Array.Empty<object>()),
-            new FormatData("{0}", new object[] { 1 }),
-            new FormatData("{0} {0}", new object[] { 1 }),
-            new FormatData("{0} {1}", new object[] { 2, 2 }),
-            new FormatData("{0} {1} {0}", new object[] { 2, 2 }),
-            new FormatData("{0} {1} {2}", new object[] { 1, 2, 3 }),
+            new TestCaseData("No argument", null),
+            new TestCaseData("No argument", Array.Empty<object>()),
+            new TestCaseData("{0}", new object[] { 1 }),
+            new TestCaseData("{0} {0}", new object[] { 1 }),
+            new TestCaseData("{0} {1}", new object[] { 2, 2 }),
+            new TestCaseData("{0} {1} {0}", new object[] { 2, 2 }),
+            new TestCaseData("{0} {1} {2}", new object[] { 1, 2, 3 }),
         };
 
-        private static readonly FormatData[] Invalids = new[]
+        private static readonly TestCaseData[] Invalids = new[]
         {
-            new FormatData("some string", new object[] { 1 }),
-            new FormatData("{0}", null),
-            new FormatData("{1}", new object[] { 1 }),
-            new FormatData("{0} {2}", new object[] { 1, 2 }),
-            new FormatData("{0}", Array.Empty<object>()),
-            new FormatData("{0}", new object[] { 1, 2 }),
-            new FormatData("{0} {0}", null),
-            new FormatData("{0} {0}", Array.Empty<object>()),
-            new FormatData("{0} {0}", new object[] { 1, 2 }),
-            new FormatData("{0} {1}", null),
-            new FormatData("{0} {1}", Array.Empty<object>()),
-            new FormatData("{0} {1}", new object[] { 1 }),
-            new FormatData("{0} {1}", new object[] { 1, 2, 3 }),
+            new TestCaseData("some string", new object[] { 1 }),
+            new TestCaseData("{0}", null),
+            new TestCaseData("{1}", new object[] { 1 }),
+            new TestCaseData("{0} {2}", new object[] { 1, 2 }),
+            new TestCaseData("{0}", Array.Empty<object>()),
+            new TestCaseData("{0}", new object[] { 1, 2 }),
+            new TestCaseData("{0} {0}", null),
+            new TestCaseData("{0} {0}", Array.Empty<object>()),
+            new TestCaseData("{0} {0}", new object[] { 1, 2 }),
+            new TestCaseData("{0} {1}", null),
+            new TestCaseData("{0} {1}", Array.Empty<object>()),
+            new TestCaseData("{0} {1}", new object[] { 1 }),
+            new TestCaseData("{0} {1}", new object[] { 1, 2, 3 }),
         };
 
         [TestCaseSource(nameof(Valids))]
-        public void FormatHappyPath(FormatData data)
+        public void FormatHappyPath(string format, object[] args)
         {
-            Assert.DoesNotThrow(() => Ensure.Format(data.Format, data.Args, "format", "args"));
+            Assert.DoesNotThrow(() => Ensure.Format(format, args, "format", "args"));
         }
 
         [TestCaseSource(nameof(Valids))]
-        public void FormatMatchesHappyPath(FormatData data)
+        public void FormatMatchesHappyPath(string format, object[] args)
         {
-            Assert.True(Ensure.FormatMatches(data.Format, data.Args));
+            Assert.True(Ensure.FormatMatches(format, args));
         }
 
         [TestCaseSource(nameof(Invalids))]
-        public void FormatThrows(FormatData data)
+        public void FormatThrows(string format, object[] args)
         {
-            var ex = Assert.Throws<ArgumentException>(() => Ensure.Format(data.Format, data.Args, "format", "args"));
+            var ex = Assert.Throws<ArgumentException>(() => Ensure.Format(format, args, "format", "args"));
 #if DEBUG
             Console.WriteLine(ex.Message);
 #endif
         }
 
         [TestCaseSource(nameof(Invalids))]
-        public void FormatDoesNotMatch(FormatData data)
+        public void FormatDoesNotMatch(string format, object[] args)
         {
-            Assert.False(Ensure.FormatMatches(data.Format, data.Args));
-        }
-
-        public class FormatData
-        {
-            public FormatData(string format, object[]? args)
-            {
-                this.Format = format;
-                this.Args = args;
-            }
-
-            public string Format { get; }
-
-            public object[]? Args { get; }
-
-            public override string ToString()
-            {
-                var args = this.Args is null
-                               ? "null"
-                               : this.Args.Length == 0
-                                     ? "object[0]"
-                                     : string.Join(",", this.Args);
-                return $"Format: {this.Format}, Args: {args}";
-            }
+            Assert.False(Ensure.FormatMatches(format, args));
         }
     }
 }
