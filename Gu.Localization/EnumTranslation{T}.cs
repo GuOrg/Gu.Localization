4cef5e8a955d2434f29d2e0852fecb0e0d1e590c
JohanLarsson <johan_gavle1@hotmail.com>
2020-06-26 20:25:46 +0200

    EnumTranslation<T> where T : System.Enum

--- Gu.Localization/EnumTranslation{T}.cs
+++ Gu.Localization/EnumTranslation{T}.cs
@@ -1,22 +1,13 @@
-namespace Gu.Localization
+﻿namespace Gu.Localization
 {
-    using System;
     using System.Globalization;
     using System.Resources;
 
     /// <summary> Helper for creating enum translations. </summary>
     /// <typeparam name="T">An enum type.</typeparam>
     public static class EnumTranslation<T>
-        where T : struct, IComparable, IFormattable, IConvertible
+        where T : System.Enum
     {
-        static EnumTranslation()
-        {
-            if (!typeof(T).IsEnum)
-            {
-                throw new InvalidOperationException("T must be an enum when used in EnumTranslation<T>. Can't constrain it better this.");
-            }
-        }
-
         /// <summary> Create a translation for <paramref name="member"/>. </summary>
         /// <param name="resourceManager">The <see cref="ResourceManager"/> with translations for <paramref name="member"/>.</param>
         /// <param name="member"> The member to translate.</param>
