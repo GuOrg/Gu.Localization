07fdee4b0e568d83c272caf0577cc727913995ef
JohanLarsson <johan_gavle1@hotmail.com>
2021-03-03 12:51:09 +0100

    [assembly: SuppressMessage("Performance", "CA1824", Justification = "Tests assume this. Don't have time to change now.")]

--- Gu.Localization.Tests/AssemblyAttributes.cs
+++ Gu.Localization.Tests/AssemblyAttributes.cs
@@ -1,3 +1,5 @@
-using System;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 [assembly: CLSCompliant(false)]
+[assembly: SuppressMessage("Performance", "CA1824", Justification = "Tests assume this. Don't have time to change now.")]
