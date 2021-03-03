3b8aa2a34060299d55b2bcb405fa8ceca16ca436
JohanLarsson <johan_gavle1@hotmail.com>
2021-03-03 12:35:12 +0100

    [assembly: SuppressMessage("Performance", "CA1824", Justification = "Tests assume this. Don't have time to change now.")]

--- Gu.Wpf.Localization.Demo.Controls/Properties/AssemblyAttributes.cs
+++ Gu.Wpf.Localization.Demo.Controls/Properties/AssemblyAttributes.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 
 [assembly: CLSCompliant(false)]
-[assembly:ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: SuppressMessage("Performance", "CA1824", Justification = "Tests assume this. Don't have time to change now.")]
