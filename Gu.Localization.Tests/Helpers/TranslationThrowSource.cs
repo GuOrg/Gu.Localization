b424d887c13233b04e6448759d3f597c03b0c04e
JohanLarsson <johan_gavle1@hotmail.com>
2021-11-25 10:44:26 +0100

    Format

--- Gu.Localization.Tests/Helpers/TranslationThrowSource.cs
+++ Gu.Localization.Tests/Helpers/TranslationThrowSource.cs
@@ -13,27 +13,50 @@
             this.Add(null, Sv, ErrorHandling.Throw, "key is null\r\nParameter name: key");
             this.Add(null, null, ErrorHandling.Throw, "key is null\r\nParameter name: key");
 #pragma warning disable SA1118 // Parameter should not span multiple lines
-            this.Add("Missing", Sv, ErrorHandling.Throw, "The ResourceManager Gu.Localization.Tests.Properties.Resources does not have the key: Missing\r\n" +
-                                                         "Fix the problem by adding a translation for the key 'Missing'\r\n" +
-                                                         "Parameter name: key");
-            this.Add("Missing", null, ErrorHandling.Throw, "The ResourceManager Gu.Localization.Tests.Properties.Resources does not have the key: Missing\r\n" +
-                                                           "Fix the problem by adding a translation for the key 'Missing'\r\n" +
-                                                           "Parameter name: key");
-            this.Add(nameof(Properties.Resources.EnglishOnly), Sv, ErrorHandling.Throw, "The ResourceManager Gu.Localization.Tests.Properties.Resources does not have a translation for the key: EnglishOnly for the culture: sv\r\n" +
-                                                                                        "Fix by either of:\r\n" +
-                                                                                        "  - Add a translation for the key 'EnglishOnly' for the culture 'sv'\r\n" +
-                                                                                        "  - If falling back to neutral is desired specify ErrorHandling.ReturnErrorInfoPreserveNeutral\r\n" +
-                                                                                        "Parameter name: key");
-            this.Add(nameof(Properties.Resources.AllLanguages), It, ErrorHandling.Throw, "The ResourceManager Gu.Localization.Tests.Properties.Resources does not have a translation for the culture: it\r\n" +
-                                                                                         "Fix by either of:\r\n" +
-                                                                                         "  - Add a resource file for the culture it\r\n" +
-                                                                                         "  - If falling back to neutral is desired specify ErrorHandling.ReturnErrorInfoPreserveNeutral\r\n" +
-                                                                                         "Parameter name: language");
-            this.Add(nameof(Properties.Resources.NeutralOnly), It, ErrorHandling.Throw, "The ResourceManager Gu.Localization.Tests.Properties.Resources does not have a translation for the culture: it\r\n" +
-                                                                                        "Fix by either of:\r\n" +
-                                                                                        "  - Add a resource file for the culture it\r\n" +
-                                                                                        "  - If falling back to neutral is desired specify ErrorHandling.ReturnErrorInfoPreserveNeutral\r\n" +
-                                                                                        "Parameter name: language");
+            this.Add(
+                "Missing",
+                Sv,
+                ErrorHandling.Throw,
+                "The ResourceManager Gu.Localization.Tests.Properties.Resources does not have the key: Missing\r\n" +
+                "Fix the problem by adding a translation for the key 'Missing'\r\n" +
+                "Parameter name: key");
+
+            this.Add(
+                "Missing",
+                null,
+                ErrorHandling.Throw,
+                "The ResourceManager Gu.Localization.Tests.Properties.Resources does not have the key: Missing\r\n" +
+                "Fix the problem by adding a translation for the key 'Missing'\r\n" +
+                "Parameter name: key");
+
+            this.Add(
+                nameof(Properties.Resources.EnglishOnly),
+                Sv,
+                ErrorHandling.Throw,
+                "The ResourceManager Gu.Localization.Tests.Properties.Resources does not have a translation for the key: EnglishOnly for the culture: sv\r\n" +
+                "Fix by either of:\r\n" +
+                "  - Add a translation for the key 'EnglishOnly' for the culture 'sv'\r\n" +
+                "  - If falling back to neutral is desired specify ErrorHandling.ReturnErrorInfoPreserveNeutral\r\n" +
+                "Parameter name: key");
+            this.Add(
+                nameof(Properties.Resources.AllLanguages),
+                It,
+                ErrorHandling.Throw,
+                "The ResourceManager Gu.Localization.Tests.Properties.Resources does not have a translation for the culture: it\r\n" +
+                "Fix by either of:\r\n" +
+                "  - Add a resource file for the culture it\r\n" +
+                "  - If falling back to neutral is desired specify ErrorHandling.ReturnErrorInfoPreserveNeutral\r\n" +
+                "Parameter name: language");
+
+            this.Add(
+                nameof(Properties.Resources.NeutralOnly),
+                It,
+                ErrorHandling.Throw,
+                "The ResourceManager Gu.Localization.Tests.Properties.Resources does not have a translation for the culture: it\r\n" +
+                "Fix by either of:\r\n" +
+                "  - Add a resource file for the culture it\r\n" +
+                "  - If falling back to neutral is desired specify ErrorHandling.ReturnErrorInfoPreserveNeutral\r\n" +
+                "Parameter name: language");
 #pragma warning restore SA1118 // Parameter should not span multiple lines
         }
 
