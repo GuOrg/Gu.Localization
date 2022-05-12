eca3dc6ab86275bae30004d6e521408af0d7b8f2
JohanLarsson <johan_gavle1@hotmail.com>
2022-05-12 11:50:19 +0200

    Fix build

--- Gu.Localization/Properties/AssemblyAttributes.cs
+++ Gu.Localization/Properties/AssemblyAttributes.cs
@@ -4,3 +4,56 @@ using System.Runtime.CompilerServices;
 [assembly: CLSCompliant(false)]
 [assembly: InternalsVisibleTo("Gu.Wpf.Localization, PublicKey=00240000048000009400000006020000002400005253413100040000010001008F3FADDAC2AF5D3A713943D1661FF19562CB93322F985E0A9EE5EA8FBA5D2DE865E27EF3F90B85AE33EABE89449BA2EF391046E9E55080F4E895AA51C298A810445F8C2E00A3EDE5AD9750801E0120421F2747BEDC112D082001C10F3255E33CC7DC4D0263792B195CFCF935C5D723EB85A4906C4B9C5B9D1FC980DE1594FE98", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Localization.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001008F3FADDAC2AF5D3A713943D1661FF19562CB93322F985E0A9EE5EA8FBA5D2DE865E27EF3F90B85AE33EABE89449BA2EF391046E9E55080F4E895AA51C298A810445F8C2E00A3EDE5AD9750801E0120421F2747BEDC112D082001C10F3255E33CC7DC4D0263792B195CFCF935C5D723EB85A4906C4B9C5B9D1FC980DE1594FE98", AllInternalsVisible = true)]
+
+#if NET48
+#pragma warning disable SA1402, SA1502, SA1600, SA1649, GU0073
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
+    internal sealed class AllowNullAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
+    internal sealed class DisallowNullAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
+    internal sealed class MaybeNullAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
+    internal sealed class NotNullAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class MaybeNullWhenAttribute : Attribute
+    {
+        public MaybeNullWhenAttribute(bool returnValue) => this.ReturnValue = returnValue;
+
+        public bool ReturnValue { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class NotNullWhenAttribute : Attribute
+    {
+        public NotNullWhenAttribute(bool returnValue) => this.ReturnValue = returnValue;
+
+        public bool ReturnValue { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
+    internal sealed class NotNullIfNotNullAttribute : Attribute
+    {
+        public NotNullIfNotNullAttribute(string parameterName) => this.ParameterName = parameterName;
+
+        public string ParameterName { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    internal sealed class DoesNotReturnAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class DoesNotReturnIfAttribute : Attribute
+    {
+        public DoesNotReturnIfAttribute(bool parameterValue) => this.ParameterValue = parameterValue;
+
+        public bool ParameterValue { get; }
+    }
+}
+#endif
