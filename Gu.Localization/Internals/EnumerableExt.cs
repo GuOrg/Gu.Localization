8ffa6ae36ece48f8f80f930c0bb6702b72c4a67e
JohanLarsson <johan_gavle1@hotmail.com>
2022-05-16 09:42:03 +0200

    Fix warnings.

--- Gu.Localization/Internals/EnumerableExt.cs
+++ Gu.Localization/Internals/EnumerableExt.cs
@@ -1,39 +1,22 @@
 ﻿namespace Gu.Localization
 {
+    using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
 
     internal static class EnumerableExt
     {
-        internal static IEnumerable<TSource> Append<TSource>(this IEnumerable<TSource> source, TSource element)
-        {
-            Debug.Assert(source != null, "source == null");
-            return AppendIterator(source, element);
-        }
-
         internal static IEnumerable<TSource> Prepend<TSource>(this IEnumerable<TSource> source, TSource element)
         {
-            Debug.Assert(source != null, "source == null");
-            return PrependIterator(source, element);
-        }
+            return PrependIterator(source ?? throw new ArgumentNullException(nameof(source)), element);
 
-        private static IEnumerable<TSource> AppendIterator<TSource>(IEnumerable<TSource> source, TSource element)
-        {
-            foreach (var e1 in source)
+            static IEnumerable<TSource> PrependIterator(IEnumerable<TSource> source, TSource element)
             {
-                yield return e1;
-            }
+                yield return element;
 
-            yield return element;
-        }
-
-        private static IEnumerable<TSource> PrependIterator<TSource>(IEnumerable<TSource> source, TSource element)
-        {
-            yield return element;
-
-            foreach (var e1 in source)
-            {
-                yield return e1;
+                foreach (var item in source)
+                {
+                    yield return item;
+                }
             }
         }
     }
